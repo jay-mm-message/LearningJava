d28007c250a45ac7bd3cad37eb4c4890a2f10dbe
jay-mm-message <jay.mm.message@gmail.com>
2024-06-15 14:18:33 +0800

    learning JavaDoc create

---
+++ exercise_example/ch04/TestJavaDoc.java
@@ -0,0 +1,21 @@
+
+/**
+ * I think this class will be the most amazing one you've ever seen.
+ * class, Let me explain my personal opinion, encourage
+ * Everyone, establish
+ * <p>
+ * It all started in my childhood, growing up on the streets of Idaho, 
+ * Potatoes are very popular, life is wonderful...
+ * 
+ * @see TestJavaDoc
+ * @author Jay
+ * @version 1.00, 15, Jun 2024
+ */
+public class TestJavaDoc {
+    /**
+     * @param args
+     */
+    public static void main( String [] args) {
+        System.out.println("Test");
+    }
+}
